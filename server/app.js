dbe8111838f5fa9ce6c70901e8cab71bfb3526a9
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-19 14:46:04 +0300

    mongoose is connected

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
 
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true    
+}).then(() => {
+    console.log("mongoose is connected")
+}).catch(err => {
+    console.error(err)
+})
 
 app.get("/",(req,res) => {
     res.send("naber")
