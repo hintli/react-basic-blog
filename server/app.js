874cdb510b6c9d86672da550cb90c06b23d7d45f
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-20 19:42:46 +0300

    logout

--- server/app.js
+++ server/app.js
@@ -94,6 +94,21 @@ app.get("/",(req,res) => {
     res.send("naber")
 })
 
+
+
+app.get("/api/user/logout", auth, (req,res) =>{
+    User.findOneAndUpdate({_id: req.user._id}, { token: ""}, (err, doc)=>{
+        if(err) return res.json({ success: false, err })
+        return res.status(200).send({
+          success: true
+        })
+    })
+  })
+  
+  
+
+
+
 app.listen(5000,() => {
     console.log("Server is running")
 })
