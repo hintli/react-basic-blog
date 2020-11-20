5c483174e38406ac3548ae9917f9e1f9671fad9c
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-20 13:32:30 +0300

    user saver register

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+// const bodyParser = require("body-parser")
+// const cookieParser = require("cookie-parser")
 const dotenv = require("dotenv")
 dotenv.config()
+
+
 
 mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
@@ -14,6 +18,30 @@ mongoose.connect(process.env.MONGO_URI,{
 }).catch(err => {
     console.error(err)
 })
+
+
+
+const {User} = require("./models/user")
+
+// app.use(bodyParser.urlencoded({ extended : true })); 
+// app.use(bodyParser.json());
+app.use(express.json())
+
+// app.use(cookieParser())
+
+
+
+app.post("/api/users/register",(req,res) => {
+    const user = new User(req.body);
+
+    user.save((err,doc) => {
+        if(err) {
+            return res.status(404).json({err})
+        }
+        return res.json({messages:true,user:doc})
+    })
+})
+
 
 app.get("/",(req,res) => {
     res.send("naber")
