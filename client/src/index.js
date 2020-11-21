a0814a0662726d951461435ba366b4b408e092d3
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-21 12:26:18 +0300

    redux

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,28 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+
+import Reducer from './reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 ReactDOM.render(
+  <Provider 
+  store={createStoreWithMiddleware( 
+  Reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__()
+)}
+>
   <BrowserRouter>
     <App />
-    </BrowserRouter>,
+    </BrowserRouter>
+    </Provider>,
   document.getElementById('root')
 );
 
